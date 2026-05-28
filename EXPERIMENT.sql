c31fa0a29c48fa72ae5f991b5a9a6519c1ea27d2
SNEHA SATPATHI <snehasatpathi75@gmail.com>
2026-05-28 14:08:15 +0530

    Update EXPERIMENT.sql

--- EXPERIMENT.sql
+++ EXPERIMENT.sql
@@ -1,13 +1,25 @@
+-- Select Database
 USE EMPLOYEE;
 
+-- Create Backup Table from EMPLOYEE
+CREATE TABLE EMPLOYEE_MASTER AS
+(
+    SELECT * FROM EMPLOYEE
+);
 
-CREATE TABLE 
-       EMPLOYEE_MASTER 
-       AS
-           (SELECT *fROM EMPLOYEE);
-           
+-- Delete Employees from Department 10
+DELETE FROM EMPLOYEE_MASTER
+WHERE DEPTNO = 10;
 
-DELETE 
-   FROM 
-      EMPLOYEE_MASTER 
-			WHERE DEPTNO = 10;
+-- Increase Salary by 10% for Department 20
+UPDATE EMPLOYEE_MASTER
+SET SAL = SAL + (SAL * 0.10)
+WHERE DEPTNO = 20;
+
+-- Modify Salary Datatype
+ALTER TABLE EMPLOYEE_MASTER
+MODIFY SAL DECIMAL(10,2);
+
+-- Delete Table Permanently
+DROP TABLE EMPLOYEE_MASTER;
+            
