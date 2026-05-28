aa13b57ae998d8a2b8885e8f211865d8573976d8
SNEHA SATPATHI <snehasatpathi75@gmail.com>
2026-05-28 14:09:02 +0530

    Update EXPERIMENT2.sql

--- EXPERIMENT2.sql
+++ EXPERIMENT2.sql
@@ -1,35 +1,81 @@
+-- Use Database
 USE EMPLOYEE;
 
+
+-- 1. List all distinct jobs in Employee
 SELECT DISTINCT JOB
-            FROM EMPLOYEE;
-            
+FROM EMPLOYEE;
 
 
-SELECT * FROM EMPLOYEE WHERE DEPTNO = 30;
+-- 2. List all information about employees 
+-- in Department Number 30
+SELECT *
+FROM EMPLOYEE
+WHERE DEPTNO = 30;
 
 
-SELECT * FROM DEPARTMENT WHERE DEPTNO > 20;
+-- 3. Find all department numbers with  department names
+--  greater than 20
+SELECT DEPTNO, DNAME
+FROM DEPARTMENT
+WHERE DEPTNO > 20;
 
 
-SELECT * FROM EMPLOYEE WHERE JOB IN ('MANAGER','CLERK') AND DEPTNO = 30;
+-- 4. Find all information about all the 
+--    managers as well as the clerks 
+--    in department 30
 
-SELECT EMPNO, ENAME, DEPTNO FROM EMPLOYEE WHERE JOB = 'CLERK';
-
-SELECT * FROM EMPLOYEE WHERE JOB = 'MANAGER' AND DEPTNO <> 30;
-
-
-SELECT * FROM EMPLOYEE  WHERE JOB NOT IN ('MANAGER','CLERK') AND DEPTNO = 10;
-
-
-SELECT EMPNO, ENAME, JOB FROM EMPLOYEE WHERE SAL BETWEEN 1200 AND 1400;
+SELECT *
+FROM EMPLOYEE
+WHERE JOB IN ('MANAGER', 'CLERK')
+AND DEPTNO = 30;
 
 
-SELECT ENAME, DEPTNO FROM EMPLOYEE WHERE JOB IN ('CLERK', 'ANALYST', 'SALESMAN');
+-- 5. List Employee name, Employee number 
+--    and department of all clerks
+
+SELECT ENAME, EMPNO, DEPTNO
+FROM EMPLOYEE
+WHERE JOB = 'CLERK';
 
 
+-- 6. Find all managers not in department 30
 
-SELECT * FROM EMPLOYEE WHERE ENAME LIKE 'M%';
+SELECT *
+FROM EMPLOYEE
+WHERE JOB = 'MANAGER'
+AND DEPTNO <> 30;
 
 
+-- 7. List information about all Employees 
+--    in department 10 who are not managers 
+--    or clerks
+
+SELECT *
+FROM EMPLOYEE
+WHERE DEPTNO = 10
+AND JOB NOT IN ('MANAGER', 'CLERK');
 
 
+-- 8. Find Employees and jobs earning 
+--    between 1200 and 1400
+
+SELECT ENAME, JOB, SAL
+FROM EMPLOYEE
+WHERE SAL BETWEEN 1200 AND 1400;
+
+
+-- 9. List Name and Department Number of employee 
+--    who are clerks, analyst or salesman
+
+SELECT ENAME, DEPTNO
+FROM EMPLOYEE
+WHERE JOB IN ('CLERK', 'ANALYST', 'SALESMAN');
+
+
+-- 10. List Name and Department Number of employee 
+--     whose names begin with M
+
+SELECT ENAME, DEPTNO
+FROM EMPLOYEE
+WHERE ENAME LIKE 'M%';
